bedd97321150456b894928025eb4d89c0902a215
ghamlet <114679713+ghamlet@users.noreply.github.com>
2023-05-12 17:57:00 +0300

    Add files via upload

---
+++ esc.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+#define esc 8
+//
+Servo motor;
+ 
+void setup() {
+  motor.attach(esc);
+  
+  
+  Serial.begin(9600);
+}
+  
+  
+
+void loop() {
+int val = map(analogRead(0), 0, 1023, 1000, 1500);
+//motor.writeMicroseconds(val); // 1230 
+Serial.println(val);
+
+  
+
+      
+
+
+}
